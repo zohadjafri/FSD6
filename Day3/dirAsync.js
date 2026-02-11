e9cfc017402ca5f07dbb9a41c82e8a7430b38062
zohadjafri <zohadjafri670@gmail.com>
2026-02-11 15:30:31 +0530

    Day3

--- Day3/dirAsync.js
+++ Day3/dirAsync.js
@@ -1,2 +1,35 @@
+dirAsync.js
 
-[{"id":1,"name":"A"},{"id":2,"name":"B"}]
+import { read } from 'fs';
+import fs from 'fs/promises';
+
+const makeDir = async(path) => {
+    try {
+        await fs.mkdir(path);
+        console.log("Directory has been created successfully");
+    } catch (error) {
+        console.log("Some error occured during mking directory");  
+    }
+} 
+
+const readDir = async(path) => {
+    try {
+        await fs.readdir(path);
+        console.log("Directory has been read successfully");
+    } catch (error) {
+        console.log("Some error occured during reading the directory");
+    }
+}
+
+const remDir = async(path) => {
+    try {
+        await fs.rmdir(path);
+        console.log("Directory has been removed successfully");
+    } catch (error) {
+        console.log("Some error occured while  removing the dir");
+    }
+}
+
+makeDir("../day_10");
+readDir("./");
+remDir("../day_10");
